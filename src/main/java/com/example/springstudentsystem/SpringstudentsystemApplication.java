72273ba9e742c39fffc604d5b38e80229797636b
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-09-09 21:46:05 +0530

    new user update 2

--- src/main/java/com/example/springstudentsystem/SpringstudentsystemApplication.java
+++ src/main/java/com/example/springstudentsystem/SpringstudentsystemApplication.java
@@ -1,7 +1,9 @@
 package com.example.springstudentsystem;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringstudentsystemApplication {
@@ -9,6 +11,9 @@ public class SpringstudentsystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringstudentsystemApplication.class, args);
 	}
-
+@Bean
+	public ModelMapper modelMapper(){
+		return  new ModelMapper();
+}
 }
 
--- src/main/java/com/example/springstudentsystem/service/UserService.java
+++ src/main/java/com/example/springstudentsystem/service/UserService.java
@@ -1,10 +1,24 @@
 package com.example.springstudentsystem.service;
 
+import com.example.springstudentsystem.dto.UserDTO;
+import com.example.springstudentsystem.entity.User;
+import com.example.springstudentsystem.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+   @Autowired
+   private ModelMapper modelMapper;
+
+    public UserDTO saveUser(UserDTO userDTO){
+        userRepository.save(modelMapper.map(userDTO, User.class));
+        return userDTO;
+    }
 
 }
